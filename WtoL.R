3e4ead222d69b8e55d27f5e38bf4c7fedffce422
GenDoc94 <juanjose.dominguez@scsalud.es>
2025-09-15 19:00:32 +0200

    Starting with the app

--- WtoL.R
+++ WtoL.R
@@ -4,28 +4,36 @@ library(writexl)
 
 #Pasar de wide a long
 
-db_wide <- read_excel("GenDB.xlsx") %>%
-        mutate(
-                Id = as.integer(Id),
-                fechanac = format(as.Date(fechanac), "%d/%m/%Y"),
-                fechapet = format(as.Date(fechapet), "%d/%m/%Y"),
-        )
-
-db_long <- db_wide %>%
-        pivot_longer(
-                cols = matches("^(gen|g_c|g_nm|g_p|g_imp|g_vaf|g_depth|g_tier)[0-9]+$"),
-                names_to = c(".value", "orden"),
-                names_pattern = "(.+?)([0-9]+)"
-        ) %>%
-        mutate(
-                orden = as.integer(orden),
-                g_depth = as.integer(g_depth),
-                g_vaf   = as.numeric(g_vaf)
-        )
-
-db_long_clean <- db_long %>%
-        filter(!(orden != 1 & (is.na(gen) | gen == "")))
-
-
-write_xlsx(db_long, "GenDB_long.xlsx")
-write_xlsx(db_long_clean, "GenDB_long_clean.xlsx")
+wide_to_long <- function(file_path) {
+        
+        # Leer Excel
+        db_wide <- read_excel(file_path) %>%
+                mutate(
+                        Id = as.integer(Id),
+                        fechanac = format(as.Date(fechanac), "%d/%m/%Y"),
+                        fechapet = format(as.Date(fechapet), "%d/%m/%Y")
+                )
+        
+        # Transformar a long
+        db_long <- db_wide %>%
+                pivot_longer(
+                        cols = matches("^(gen|g_c|g_nm|g_p|g_imp|g_vaf|g_depth|g_tier)[0-9]+$"),
+                        names_to = c(".value", "orden"),
+                        names_pattern = "(.+?)([0-9]+)"
+                ) %>%
+                mutate(
+                        orden = as.integer(orden),
+                        g_depth = as.integer(g_depth),
+                        g_vaf = as.numeric(g_vaf)
+                )
+        
+        # Limpiar base
+        db_long_clean <- db_long %>%
+                filter(!(orden != 1 & (is.na(gen) | gen == "")))
+        
+        # Opcional: guardar en disco
+        write_xlsx(db_long, "GenDB_long.xlsx")
+        write_xlsx(db_long_clean, "GenDB_long_clean.xlsx")
+        
+        return(db_long_clean)
+}
--- app.R
+++ app.R
@@ -1,51 +1,54 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
+library(dplyr)
+library(readxl)
 
-# Define UI for application that draws a histogram
+# Cargar script externo para wide -> long (opcional)
+source("WtoL.R")
+
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+        titlePanel("App de Ejemplo"),
+        sidebarLayout(
+                sidebarPanel(
+                        fileInput("file", "Sube un archivo Excel", accept = c(".xlsx")),
+                        br(),
+                        p("This is a simple application to perform descriptive statistics on NGS data.")
+                ),
+                mainPanel(
+                        tabsetPanel(
+                                tabPanel("Data",
+                                         tableOutput("table_preview")
+                                ),
+                                tabPanel("Descriptive",
+                                         p("")  # Vacía
+                                ),
+                                tabPanel("En blanco",
+                                         p("")  # Vacía
+                                )
+                        )
+                )
         )
-    )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+server <- function(input, output, session) {
+        
+        # Reactive para df_wide
+        db_wide <- reactive({
+                req(input$file)
+                read_excel(input$file$datapath) %>%
+                        mutate(
+                                Id = as.integer(Id),
+                                fechanac = format(as.Date(fechanac), "%d/%m/%Y"),
+                                fechapet = format(as.Date(fechapet), "%d/%m/%Y")
+                        )
+        })
+        
+        # Output Data (df_wide)
+        output$table_preview <- renderTable({
+                df <- db_wide()  # Llamar al reactivo
+                if(is.null(df)) return(NULL)
+                
+                df %>% select(1:5) %>% head(5)
+        })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
